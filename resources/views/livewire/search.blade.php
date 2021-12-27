4202e1da3c75d8f41187390dc81fd95d7181adca
laraveller <laraveller2021@gmail.com>
2021-12-27 17:09:56 +0100

    loading state

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -10,11 +10,27 @@
     <x-jet-dialog-modal wire:model="showSearchModal">
         <x-slot name="title">Search Movies</x-slot>
         <x-slot name="content">
+
             <div class="flex flex-col">
                 <input wire:model="search" type="text" class="rounded w-full dark:bg-gray-700"
                     placeholder="Search Movie">
+                <div wire:loading class="border border-blue-300 shadow rounded-md p-4 w-full mx-auto mt-4">
+                    <div class="animate-pulse flex space-x-4">
+                        <div class="rounded-full bg-gray-200 h-10 w-10"></div>
+                        <div class="flex-1 space-y-6 py-1">
+                            <div class="h-2 bg-gray-200 rounded"></div>
+                            <div class="space-y-3">
+                                <div class="grid grid-cols-3 gap-4">
+                                    <div class="h-2 bg-gray-200 rounded col-span-2"></div>
+                                    <div class="h-2 bg-gray-200 rounded col-span-1"></div>
+                                </div>
+                                <div class="h-2 bg-gray-200 rounded"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 @if (!empty($search))
-                    <div class="">
+                    <div class="" wire:loading.remove>
                         @if (!empty($searchResults))
                             @foreach ($searchResults->groupByType() as $type => $modelSearchResults)
                                 <h1>{{ $type }}</h1>
